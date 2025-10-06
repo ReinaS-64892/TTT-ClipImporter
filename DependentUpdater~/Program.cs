0ca1e5a2930f416ff5dc1ee7a3347576ea41c4b5
Reina_Sakiria <rs@rs64.net>
2025-10-06 20:17:13 +0900

    chore: create DependencyUpdater

---
+++ DependentUpdater~/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Write dependency version start");
+
+Directory.SetCurrentDirectory("../"); // move to repository root
+const string TEX_TRANS_CORE_PACKAGE_DOT_JSON_PATH = "../TexTransTool/package.json";
+var texTransCorePackageJson = System.Text.Json.Nodes.JsonNode.Parse(File.ReadAllText(TEX_TRANS_CORE_PACKAGE_DOT_JSON_PATH));
+if (texTransCorePackageJson is null) { throw new NullReferenceException(); }
+var tttVersion = texTransCorePackageJson["version"]!.GetValue<string>();
+var tttCode = texTransCorePackageJson["name"]!.GetValue<string>();
+
+
+var tttPackageJsonPath = @"package.json";
+var tttPackageJson = System.Text.Json.Nodes.JsonNode.Parse(File.ReadAllText(tttPackageJsonPath));
+if (tttPackageJson is null) { throw new NullReferenceException(); }
+
+tttPackageJson["dependencies"]![tttCode] = tttVersion;
+tttPackageJson["vpmDependencies"]![tttCode] = "^" + tttVersion;
+
+
+var outOpt = new System.Text.Json.JsonSerializerOptions(System.Text.Json.JsonSerializerDefaults.General) { WriteIndented = true };
+File.WriteAllText(tttPackageJsonPath, tttPackageJson.ToJsonString(outOpt) + "\n");
+Console.WriteLine("Write version exit!");
